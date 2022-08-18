d3123cbf15d7ac9862966b4cde78584c06491278
hugo <hugo.monmousseau@hotmail.fr>
2022-08-18 11:46:03 +0200

    AllCards

--- AutoChessLucas/Assets/Script/GameManager.cs
+++ AutoChessLucas/Assets/Script/GameManager.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+
+    public List<string> allCards = new List<string>();
     private void Awake()
     {
         Instance = this;
+
     }
     void Start()
     {
